1aa35d8f96cb7a5245b4f2d681c258c5ce9a501c
Renato Almeida <renato.p.almeida@gmail.com>
2019-11-20 14:55:11 -0300

    Obtendo um usuário por id e tratamento de exceção particular

--- src/main/java/com/rparenato/cursoSpringBootMongoDB/resources/UserResource.java
+++ src/main/java/com/rparenato/cursoSpringBootMongoDB/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +17,22 @@ import com.rparenato.cursoSpringBootMongoDB.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<UserDTO>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		UserDTO userDTO = new UserDTO(user);
+		return ResponseEntity.ok().body(userDTO);
+	}
+
 }
--- src/main/java/com/rparenato/cursoSpringBootMongoDB/services/UserService.java
+++ src/main/java/com/rparenato/cursoSpringBootMongoDB/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rparenato.cursoSpringBootMongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rparenato.cursoSpringBootMongoDB.domain.User;
 import com.rparenato.cursoSpringBootMongoDB.repository.UserRepository;
+import com.rparenato.cursoSpringBootMongoDB.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
