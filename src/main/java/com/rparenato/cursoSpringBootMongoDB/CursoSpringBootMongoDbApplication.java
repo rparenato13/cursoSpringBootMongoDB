69f420f4a877811f2b9e0696dcca2d58c23fc940
Renato Almeida <renato.p.almeida@gmail.com>
2019-11-20 13:24:29 -0300

    Primeiro commit: Projeto criado

---
+++ src/main/java/com/rparenato/cursoSpringBootMongoDB/CursoSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.rparenato.cursoSpringBootMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rparenato/cursoSpringBootMongoDB/CursoSpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rparenato.cursoSpringBootMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
