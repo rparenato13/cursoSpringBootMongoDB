bdb89fdad74273920f8f5604e6a38b93379d86a8
Renato Almeida <renato.p.almeida@gmail.com>
2019-11-21 21:25:36 -0300

    Obtendo um post por id

---
+++ src/main/java/com/rparenato/cursoSpringBootMongoDB/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.rparenato.cursoSpringBootMongoDB.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rparenato.cursoSpringBootMongoDB.domain.Post;
+import com.rparenato.cursoSpringBootMongoDB.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+
+}
---
+++ src/main/java/com/rparenato/cursoSpringBootMongoDB/services/PostService.java
@@ -0,0 +1,23 @@
+package com.rparenato.cursoSpringBootMongoDB.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rparenato.cursoSpringBootMongoDB.domain.Post;
+import com.rparenato.cursoSpringBootMongoDB.repository.PostRepository;
+import com.rparenato.cursoSpringBootMongoDB.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
