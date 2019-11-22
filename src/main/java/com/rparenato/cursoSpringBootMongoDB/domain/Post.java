b4d1e2e67f15761e69ebf9770f84b5764ea8825f
Renato Almeida <renato.p.almeida@gmail.com>
2019-11-21 21:38:25 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/rparenato/cursoSpringBootMongoDB/domain/Post.java
+++ src/main/java/com/rparenato/cursoSpringBootMongoDB/domain/Post.java
@@ -1,12 +1,15 @@
 package com.rparenato.cursoSpringBootMongoDB.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rparenato.cursoSpringBootMongoDB.dto.AuthorDTO;
+import com.rparenato.cursoSpringBootMongoDB.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,7 @@ public class Post implements Serializable {
 	private String body;
 
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -70,6 +74,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
